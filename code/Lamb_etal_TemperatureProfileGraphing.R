45a00c29fc418ad4a636cfe9911b315baf8f70df
AnnikaMLamb <160680218+AnnikaMLamb@users.noreply.github.com>
2024-09-05 15:18:33 +1000

    Update Lamb_etal_TemperatureProfileGraphing.R
    Revised colours and style of graphics during peer review

--- code/Lamb_etal_TemperatureProfileGraphing.R
+++ code/Lamb_etal_TemperatureProfileGraphing.R
@@ -27,28 +27,30 @@ Temp$DHW<-Temp$c3/7
 #Plot average daily tank temperatures
 ggplot(aes(x=Day, y=ElevatedTemperature), data=Temp)+ ylab("Mean daily temperature") + scale_x_continuous (n.breaks = 12 , expand = c(0,0),limits = c ( 1 , 66 ) ) + geom_hline(yintercept=27.8, linetype="dashed", color = "black", size = 1.2)+ geom_line(size=1.2) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                                                                                                                                                                                      panel.background = element_blank(), axis.line = element_line(colour = "black"))+theme(axis.text.x = element_text(size = 14), axis.title.x = element_text(size = 16),
-                                                                                                                                                                                                                                                                           axis.text.y = element_text(size = 14), axis.title.y = element_text(size = 16),                                                                                                                                                                                                                                                               legend.text = element_text(size = 14), legend.title = element_text(size = 14))
+                                                                                                                                                                                                                                                                           axis.text.y = element_text(size = 14), axis.title.y = element_text(size = 16),
+                                                                                                                                                                                                                                                                           legend.text = element_text(size = 14), legend.title = element_text(size = 14))
 #Plot cumulative DHWs
 ggplot(aes(x=Day, y=DHW), data=Temp)+ ylab("Cummulative degree heating weeks") + scale_x_continuous (n.breaks = 12 , expand = c(0,0),limits = c ( 1 , 66 ) ) + scale_y_continuous (n.breaks = 13 , expand = c(0,0),limits = c ( 0 , 13 ) ) + geom_line(size=1.2) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                                                                                                                                                                                                                                                                               panel.background = element_blank(), axis.line = element_line(colour = "black"))+theme(axis.text.x = element_text(size = 14), axis.title.x = element_text(size = 16),
-                                                                                                                                                                                                                                                                                                                                                                    axis.text.y = element_text(size = 14), axis.title.y = element_text(size = 16),                                                                                                                                                                                                                                                                                                                                                         legend.text = element_text(size = 14), legend.title = element_text(size = 14))
+                                                                                                                                                                                                                                                                                                                                                                    axis.text.y = element_text(size = 14), axis.title.y = element_text(size = 16),
+                                                                                                                                                                                                                                                                                                                                                                    legend.text = element_text(size = 14), legend.title = element_text(size = 14))
 
 #Combined plot of average daily temperatures and DHWs
 # Plot both lines on one set of axes
 combined_plot <- ggplot(Temp, aes(x = Day)) +
-  geom_line(aes(y = ElevatedTemperature), color = "black", size = 1.2) +
-  geom_line(aes(y = RampTemp), color = "blue", size = 1.2) +
-  geom_line(aes(y = (DHW*0.308) + 27), color = "red", size = 1.2) +  # Multiply DHW by 5 to demonstrate the secondary axis
+  geom_line(aes(y = ElevatedTemperature), color = "black", size = 1.2, linetype="dotdash") +
+  geom_line(aes(y = RampTemp), color = "blue", size = 1.5, linetype="solid") +
+  geom_line(aes(y = (DHW*0.308) + 27), color = "red", size = 1.2, linetype="dashed") +  # Multiply DHW by 5 to demonstrate the secondary axis
   scale_y_continuous(name = "Mean daily temperature", n.breaks = 6 , expand = c(0,0),limits = c(27, 31), sec.axis = sec_axis(~(.*3.25)-87.75, name = "Cumulative degree heating weeks")) +
-  geom_hline(yintercept=27.8, linetype="dashed", color = "black", size = 1.2)+
+  geom_hline(yintercept=27.8, linetype="dotted", color = "black", size = 1.2)+
    xlab("Day") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(size = 16),
-        axis.title.x = element_text(size = 18),
-        axis.text.y = element_text(size = 16),
-        axis.title.y = element_text(size = 18),
-        legend.text = element_text(size = 16),
-        legend.title = element_text(size = 16),
+  theme_classic() +
+  theme(axis.text.x = element_text(size = 18),
+        axis.title.x = element_text(size = 20),
+        axis.text.y = element_text(size = 18),
+        axis.title.y = element_text(size = 20),
+        legend.text = element_text(size = 18),
+        legend.title = element_text(size = 18),
         axis.line = element_line(color = "black"),
         axis.line.y.right = element_line(color = "red"), 
         axis.ticks.y.right = element_line(color = "red"),
